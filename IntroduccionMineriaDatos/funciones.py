90ef1dcc3ba9f67314ad3ced3bd8bc6a45fd5173
Alberto Luque Rivas <i62luria@uco.es>
2019-09-11 17:22:37 +0200

    Primer Commit

---
+++ IntroduccionMineriaDatos/funciones.py
@@ -0,0 +1,18 @@
+
+def suma(a,b):
+    """
+    Sumasion de dos valores
+
+        """
+    return a+b
+def palabras(a):
+    """
+
+    :param a:
+    :return:
+    """
+    b = a.split();
+
+    print (len(b))
+
+    return
---
+++ IntroduccionMineriaDatos/main.py
@@ -0,0 +1,5 @@
+import funciones as F
+print('Ejecutando como programa principal')
+a = input('Enter your input 1:')
+
+F.palabras(a)
