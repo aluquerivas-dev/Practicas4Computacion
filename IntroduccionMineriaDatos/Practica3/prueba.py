b7a46bd0781b385bf854ab9037611af69b828a66
Alberto Luque Rivas <i62luria@uco.es>
2019-10-30 19:34:00 +0100

    P3 IMD Parte 3

--- IntroduccionMineriaDatos/Practica3/prueba.py
+++ IntroduccionMineriaDatos/Practica3/prueba.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import sklearn
 from sklearn.datasets import load_iris
@@ -13,49 +12,75 @@ from sklearn.tree import export_graphviz
 import matplotlib.pyplot as plt
 from sklearn.externals.six import StringIO
 from IPython.display import Image
+from sklearn.metrics import mean_squared_error
 import pydotplus
 from scipy.io import arff
 import pandas as pd
 from os import listdir
-def predecir(aux):
-    array = knn.predict([aux])
+import random
+def predecir(aux,clasificador):
+    array = clasificador.predict([aux])
     print('Clase predicha KNN')
     print(target[1][array][0])
     print('----------------')
 
+
+
+#MAIN
+matrix_ccr = np.array([('   ','','','',''),('KNN',1.0,0.0,0.0,0.0),('SVM',2.0,0.0,0.0,0.0),('DTC',3.0,0.0,0.0,0.0)])
+matrix_mse = np.array([('   ','','','',''),('KNN',1.0,0.0,0.0,0.0),('SVM',2.0,0.0,0.0,0.0),('DTC',3.0,0.0,0.0,0.0)])
 lista_datasets = listdir('./datasets/')
-print(lista_datasets)
+##Montar la matriz de CCR
+c = 1
+for i in lista_datasets:
+    matrix_ccr[0][c] = i
+    matrix_mse[0][c] = i
+    c = c + 1
+print(matrix_ccr)
+print(matrix_mse)
+lista_datasets = listdir('./datasets/')
+AUX = 1
 for indice in lista_datasets:
 
     dataset = arff.loadarff('./datasets/'+str(indice))
     df = pd.DataFrame(dataset[0])
     target = pd.factorize(df['class'])
-
     X_train,X_test,Y_train,Y_test=train_test_split(df.iloc[:, df.columns != 'class'],target[0], test_size=0.4)
 
+    indice_aleatorio = random.randint(1, df.shape[0])
+    patron_aleatorio = df.iloc[indice_aleatorio, df.columns != 'class']
+
     #Llamada y entrenamiento del algoritmo KNN
-    knn=KNeighborsClassifier(n_neighbors=25)
+    print('|||||||||||||||||||||||||||||KNN||||||||||||||||||||||||||||||||||||')
+    knn = KNeighborsClassifier(n_neighbors=25)
     knn.fit(X_train,Y_train)
-    print('Porcentaje de bien clasificados KNN (\''+str(indice)+'\'):')
-    print(str(knn.score(X_test,Y_test)*100)+'%')
+    print('Porcentaje de bien clasificados KNN (\''+str(indice)+'\'): '+str(knn.score(X_test,Y_test)*100)+' %')
+    print('Patron a clasificar del dataset('+str(indice_aleatorio)+')\n')
+    matrix_ccr[1][AUX] = knn.score(X_test,Y_test)
+    matrix_mse[1][AUX] = 1.0-knn.score(X_test,Y_test)
+    predecir(patron_aleatorio,knn)
 
-    if (indice == 'iris.arff'):
-        aux=[5.1, 3.5, 1.4, 0.2]
-        predecir(aux)
+    #Llamada y entrenamiento algoritmo SVM
+    print('|||||||||||||||||||||||||||||SVM||||||||||||||||||||||||||||||||||||')
+    svm = SVC(gamma='auto')
+    svm.fit(X_train, Y_train)
+    print('Porcentaje de bien clasificados SVM (\''+str(indice)+'\'): '+str(svm.score(X_test,Y_test)*100)+' %')
+    print('Patron a clasificar del dataset(' + str(indice_aleatorio) + ')\n')
+    matrix_ccr[2][AUX] = svm.score(X_test,Y_test)
+    matrix_mse[2][AUX] = 1.0-svm.score(X_test,Y_test)
+    predecir(patron_aleatorio,svm)
 
-    if (indice == 'cpu.arff'):
-        aux=[23,16000,32000,64,16,32]
-        predecir(aux)
+    # Llamada y entrenamiento algoritmo DTC
+    print('|||||||||||||||||||||||||||||DTC||||||||||||||||||||||||||||||||||||')
+    dtc=DecisionTreeClassifier()
+    dtc.fit(X_train, Y_train)
+    print('Porcentaje de bien clasificados DTC (\'' + str(indice) + '\'): ' + str(dtc.score(X_test, Y_test) * 100) + ' %')
+    print('Patron a clasificar del dataset(' + str(indice_aleatorio) + ')\n')
+    matrix_ccr[3][AUX] = dtc.score(X_test, Y_test)
+    matrix_mse[3][AUX] = 1.0-dtc.score(X_test, Y_test)
+    predecir(patron_aleatorio, dtc)
+    AUX = AUX + 1
 
-    if (indice == 'diabetes.arff'):
-        aux=[6,148,72,35,0,33.6,0.627,50]
-        predecir(aux)
-
-    if (indice == 'glass.arff'):
-        aux=[1.51689,12.67,2.88,1.71,73.21,0.73,8.54,0,0]
-        predecir(aux)
-
-    if (indice == 'ionosphere.arff'):
-        aux=[1,0,0.99539,-0.05889,0.85243,0.02306,0.83398,-0.37708,1,0.03760,0.85243,-0.17755,0.59755,-0.44945,0.60536,-0.38223,0.84356,-0.38542,0.58212,-0.32192,0.56971,-0.29674,0.36946,-0.47357,0.56811,-0.51171,0.41078,-0.46168,0.21266,-0.34090,0.42267,-0.54487,0.18641,-0.45300]
-        predecir(aux)
+print(matrix_ccr)
+print(matrix_mse)
 
