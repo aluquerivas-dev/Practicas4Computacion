8d2c971a44b98ea9549344c56c28df93f04ed2f4
Alberto Luque Rivas <i62luria@uco.es>
2019-11-26 12:04:06 +0100

    P4 IMC Parte 3

--- IntroducionModelosComputacionales/Practica4/main.py
+++ IntroducionModelosComputacionales/Practica4/main.py
@@ -1,25 +1,41 @@
 import numpy as np
 import pandas as pd
 from sklearn import svm
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.svm import SVC
 import time
-for C in [0.01,0.1,1,10]:
-    time_start = time.perf_counter()
-    optimo = svm.SVC(kernel='rbf',C=C,gamma='auto')
-    data_train = pd.read_csv('./BasesDatos/csv/train_spam.csv',header=None)
-    data_test = pd.read_csv('./BasesDatos/csv/test_spam.csv',header=None)
 
-    train_inputs = data_train.iloc[:,:-1].values
-    train_outputs = data_train.iloc[:,-1].values
+time_start = time.perf_counter()
+data_train = pd.read_csv('./BasesDatos/csv/train_spam.csv', header=None)
+data_test = pd.read_csv('./BasesDatos/csv/test_spam.csv', header=None)
+vocabulario = pd.read_csv('./etiquetasYVocabulario/vocab.txt', header=None,delimiter='\t')
+vocabulario = vocabulario.iloc[:,-1]
 
-    test_inputs = data_test.iloc[:,:-1].values
-    test_outputs = data_test.iloc[:,-1].values
+train_inputs = data_train.iloc[:, :-1].values
+train_outputs = data_train.iloc[:, -1].values
 
-    optimo.fit(train_inputs, train_outputs)
-    Y_pred = optimo.predict(test_inputs)
-    time_elapsed = (time.perf_counter() - time_start)
-    print("%5.1f segundos" % (time_elapsed))
-    print('Valor de C: ' + str(C))
-    print('Valor de score: '+str(accuracy_score(test_outputs, Y_pred)))
+test_inputs = data_test.iloc[:, :-1].values
+test_outputs = data_test.iloc[:, -1].values
+
+SVM = svm.SVC(kernel='linear', C=0.1)
+SVM.fit(train_inputs, train_outputs)
+Y_pred = SVM.predict(test_inputs)
+time_elapsed = (time.perf_counter() - time_start)
+
+print("%5.1f segundos" % (time_elapsed))
+
+print('Valor de score: ' + str(accuracy_score(test_outputs, Y_pred)))
+
+print(confusion_matrix(test_outputs,Y_pred))
+
+
+CONTADOR = 1
+for etiqueta_real, etiqueta_predicha in zip(test_outputs, Y_pred):
+    if etiqueta_real!= etiqueta_predicha:
+        print('Correo Mal Predicho: '+str(CONTADOR))
+        C=0
+        for i in (test_inputs[CONTADOR,:]):
+            if(i==1):
+                print('\t\t\tPalabra['+str(C)+']: '+str(vocabulario[C]))
+            C=C+1
+    CONTADOR=CONTADOR+1
