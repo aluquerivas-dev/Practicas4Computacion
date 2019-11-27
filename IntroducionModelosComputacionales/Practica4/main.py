fb2b7c0e8e74abdc39d0bc0c4173d0d433f8f43e
Alberto Luque Rivas <i62luria@uco.es>
2019-11-27 11:19:18 +0100

    P4 IMC Terminada

--- IntroducionModelosComputacionales/Practica4/main.py
+++ IntroducionModelosComputacionales/Practica4/main.py
@@ -1,41 +1,43 @@
 import numpy as np
 import pandas as pd
 from sklearn import svm
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
 from sklearn.svm import SVC
-import time
+import warnings
+warnings.filterwarnings('ignore')
+warnings.filterwarnings(action='ignore',category=DeprecationWarning)
+warnings.filterwarnings(action='ignore',category=FutureWarning)
 
-time_start = time.perf_counter()
-data_train = pd.read_csv('./BasesDatos/csv/train_spam.csv', header=None)
-data_test = pd.read_csv('./BasesDatos/csv/test_spam.csv', header=None)
-vocabulario = pd.read_csv('./etiquetasYVocabulario/vocab.txt', header=None,delimiter='\t')
-vocabulario = vocabulario.iloc[:,-1]
-
-train_inputs = data_train.iloc[:, :-1].values
-train_outputs = data_train.iloc[:, -1].values
-
-test_inputs = data_test.iloc[:, :-1].values
-test_outputs = data_test.iloc[:, -1].values
-
-SVM = svm.SVC(kernel='linear', C=0.1)
-SVM.fit(train_inputs, train_outputs)
-Y_pred = SVM.predict(test_inputs)
-time_elapsed = (time.perf_counter() - time_start)
-
-print("%5.1f segundos" % (time_elapsed))
-
-print('Valor de score: ' + str(accuracy_score(test_outputs, Y_pred)))
-
-print(confusion_matrix(test_outputs,Y_pred))
+tuned_parameters = [{
+                    'kernel': ['rbf'],
+                    'gamma': np.logspace(-15, 3, num=9, base=2),
+                     'C': np.logspace(-5, 15, num=11, base=2)
+                    }]
+optimo = GridSearchCV(estimator=svm.SVC(kernel='rbf'),param_grid=tuned_parameters, n_jobs=-1, cv=5)
 
 
-CONTADOR = 1
-for etiqueta_real, etiqueta_predicha in zip(test_outputs, Y_pred):
-    if etiqueta_real!= etiqueta_predicha:
-        print('Correo Mal Predicho: '+str(CONTADOR))
-        C=0
-        for i in (test_inputs[CONTADOR,:]):
-            if(i==1):
-                print('\t\t\tPalabra['+str(C)+']: '+str(vocabulario[C]))
-            C=C+1
-    CONTADOR=CONTADOR+1
+data = pd.read_csv('./BasesDatos/csv/dataset3.csv',header=None)
+
+inputs = data.iloc[:,:-1].values
+outputs = data.iloc[:,-1].values
+
+SSS = StratifiedShuffleSplit(n_splits=10, test_size=0.25,train_size=0.75)
+#En SSS.split guardamos todos los split aleatorios del dataset, usamos un for para si fuera de mas de 1 split.
+SPLIT = 1
+C_ARRAY = []
+G_ARRAY = []
+for train_index, test_index in SSS.split(inputs, outputs):
+    X_train, X_test = inputs[train_index], inputs[test_index]
+    Y_train, Y_test = outputs[train_index], outputs[test_index]
+
+    optimo.fit(X_train, Y_train)
+    Y_pred = optimo.predict(X_test)
+    print('Valor de C: '+str(optimo.best_params_['C']))
+    print('Valor de GAMMA: '+str(optimo.best_params_['gamma']))
+    print('Valor de score: '+str(accuracy_score(Y_test, Y_pred)))
+    C_ARRAY.append(optimo.best_params_['C'])
+    G_ARRAY.append(optimo.best_params_['gamma'])
+
+print("Parametro 'C' estimado medio: "+str(np.mean(C_ARRAY)))
+print("Parametro 'gamma' estimado medio: "+str(np.mean(G_ARRAY)))
